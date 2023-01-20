cdf131397de180e71b016641b6062f290014876c
Sofia G. Gallego <nefinia@gmail.com>
2023-01-20 13:42:18 -0300

    updates

---
+++ carousel.js
@@ -0,0 +1,28 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function plusSlides(n) {
+  var slides = document.getElementsByClassName("slide");
+  slideIndex += n;
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  } else if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
---
+++ menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#menu-icon").click(function() {
+        $(".nav-menu").slideToggle(); // toggle the visibility of the navigation menu
+    });
+});
